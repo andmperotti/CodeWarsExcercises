813b29cfcc363e2eb72dd23365bb043cd5bef1eb
Andrew Perotti <perotti28@gmail.com>
2023-04-03 08:15:55 -0700

    Added ConvertANumberToAString.js

---
+++ convetANumberToAString.js
@@ -0,0 +1,19 @@
+//https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+/*
+DESCRIPTION:
+We need a function that can transform a number (integer) into a string.
+
+What ways of achieving this do you know?
+
+Examples (input --> output):
+123  --> "123"
+999  --> "999"
+-100 --> "-100"
+*/
+
+function numToString(num){
+	let converted = '';
+	return converted+=num
+}
+console.log(numToString(123))
+console.log(typeof numToString(123))
