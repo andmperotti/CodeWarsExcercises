849793dd5744007983735f274a94ef8672ee9d17
Andrew Perotti <perotti28@gmail.com>
2023-04-09 07:58:09 -0700

    added todays challenge and saved the final result for yesterdays

--- sumOfPositive.js
+++ sumOfPositive.js
@@ -8,7 +8,7 @@ Example [1,-4,7,12] => 1 + 7 + 12 = 20
 Note: if there is nothing to sum, the sum is default to 0.
 */
 
-function positiveSum(arr){
+function positiveSum(arr=[0]){
 	let result = 0;
 	for(let number of arr){
 		if(Math.sign(number)>0){
