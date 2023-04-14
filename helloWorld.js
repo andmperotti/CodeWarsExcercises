9d08aa55f9ac5d2ae03f9f29c43e9c84dfa005eb
Andrew Perotti <perotti28@gmail.com>
2023-04-14 08:29:47 -0700

    added todays challenge

---
+++ helloWorld.js
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/523b4ff7adca849afe000035
+// Write a function "greet" that returns "hello world!"
+function greet(){
+  return "hello world!"
+}
+
+//I guess today was a freebie
