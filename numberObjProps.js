f0552e7afebc569d3760f2737b877231390fc25b
DrewPerotti <51537156+DrewPerotti@users.noreply.github.com>
2023-06-02 08:01:39 -0700

    added todays challenge, which I didn't know how to do, but have added to my Anki deck

---
+++ numberObjProps.js
@@ -0,0 +1,15 @@
+//https://www.codewars.com/kata/5722fd3ab7162a3a4500031f
+function whatNumberIsIt(n){
+  switch (true) {
+    case (n==Number.MAX_VALUE): return "Input number is Number.MAX_VALUE";
+    case (n==Number.POSITIVE_INFINITY): return "Input number is Number.POSITIVE_INFINITY";
+    case (n==Number.MIN_VALUE): return "Input number is Number.MIN_VALUE";
+    case (n==Number.NEGATIVE_INFINITY): return "Input number is Number.NEGATIVE_INFINITY";
+    case (isNaN(n)): return "Input number is Number.NaN";
+    default: return "Input number is " + n;
+  }
+}
+
+
+console.log(whatNumberIsIt(1/0))
+//I did not figure this out on my own, I did have a switch statement, but I failed to realize that I didn't need to make input into a number object but that they already were a number object, and that is what I got caught up on so I'll add this to my anki and will remember it from now on.
