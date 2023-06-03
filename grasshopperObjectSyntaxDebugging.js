4f96325d2f0b807dff5c1120ab9697bec878daa3
DrewPerotti <51537156+DrewPerotti@users.noreply.github.com>
2023-06-03 08:09:29 -0700

    added today's challenge which was given code that defines an object fix the errors and resumbit

---
+++ grasshopperObjectSyntaxDebugging.js
@@ -0,0 +1,37 @@
+//https://www.codewars.com/kata/56d8ae9237123036d3001b54
+//starting code:
+// var rooms = {
+//   first: {
+//     description: 'This is the first room'
+//     items: {
+//       chair: 'The old chair looks comfortable',
+//       lamp: 'This lamp looks ancient'
+//   },
+//   second: {
+//     description: 'This is the second room'
+//     items: {
+//       couch: 'This couch looks like it would hurt your back,
+//       table: 'On the table there is an unopened bottle of water'
+//     }
+//   }
+// }
+
+//my submittance code:
+var rooms = {
+	first: {
+		description: 'This is the first room',
+		items: {
+			chair: 'the old chair looks comfortable',
+			lamp: 'This lamp looks ancient'
+		}
+	},
+	second: {
+		description: 'this is the second room', 
+		items: {
+			couch: 'this couch looks like it would hurt your back',
+			table: 'On the table there is an unopened bottle of water'
+		}
+	}
+}
+
+//I looked at others solutions and mine looks the same but it doesn't pass the test they wrote, they show a quite a number of white space creating shortcutes like /n and I'm not going to type that because that's not good looking code, so maybe it's a me problem.
