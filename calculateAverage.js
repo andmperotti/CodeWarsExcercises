8e17b65903e878c2a89a16f03e8f9664f76459e8
DrewPerotti <51537156+DrewPerotti@users.noreply.github.com>
2023-06-13 09:17:49 -0700

    added todays challenge which was to create a function that given an array computes the average value of that arrays values, if it was an empty array then return +0

---
+++ calculateAverage.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/57a2013acf1fa5bfc4000921/train/javascript
+function findAverage(array) {
+	if(array.length>0){
+		return array.reduce((a,c)=>a+c, 0)/array.length
+	}if(array.length<1){
+		return +0
+	}
+}
--- numberObjProps.js
+++ numberObjProps.js
@@ -11,5 +11,5 @@ function whatNumberIsIt(n){
 }
 
 
-console.log(whatNumberIsIt(1/0))
+console.log(whatNumberIsIt(1.7976931348623157e+308-10000*2599999999999))
 //I did not figure this out on my own, I did have a switch statement, but I failed to realize that I didn't need to make input into a number object but that they already were a number object, and that is what I got caught up on so I'll add this to my anki and will remember it from now on.
