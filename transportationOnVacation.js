dbcabf8b3feef5fd43bd2fa8975932ca6acf4553
DrewPerotti <51537156+DrewPerotti@users.noreply.github.com>
2023-06-29 08:15:53 -0700

    added todays challenge which was a function that calculated a rental car cost which takes in a number of days and considers 2 duration discounts while calculating the total cost of the rental

---
+++ transportationOnVacation.js
@@ -0,0 +1,13 @@
+//https://www.codewars.com/kata/568d0dd208ee69389d000016
+function rentalCarCost(d) {
+  if(d>=7){
+  	return d*40-50
+  }
+  if(d>=3){
+  	return d*40-20
+  }
+  else{
+  	return d*40
+  }
+}
+console.log(rentalCarCost(7))
