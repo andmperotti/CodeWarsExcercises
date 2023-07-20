27340d5e5cdcd65b754e73aaa3e7700050eb86e5
DrewPerotti <51537156+DrewPerotti@users.noreply.github.com>
2023-07-20 09:01:46 -0700

    added todays challenge which was to build a simple function that made an array of numbers starting at 1 and ending with whatever number you give the function

---
+++ countTheMonkey.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/56f69d9f9400f508fb000ba7
+function monkeyCount(n) {
+  let resultArr =[]
+  for(let i=1; i<=n; i++){
+    resultArr.push(i)
+  }
+  return resultArr
+}
