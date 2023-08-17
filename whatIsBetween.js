4b86d449cf96611b89cabe65c475261dd4b212ff
Andrew Perotti <Perotti28@gmail.com>
2023-08-17 08:20:34 -0700

    added todays challenge which was to build a function that takes in two parameters and builds an array of integers between those 2 parameters and including them

---
+++ whatIsBetween.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/55ecd718f46fba02e5000029
+function between(a, b) {
+	let result = []
+	for(let i = a; i<=b; i++){
+		result.push(i)
+	}
+	return result
+}
+
+console.log(between(3,8))
