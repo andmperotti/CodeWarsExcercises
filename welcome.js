d97a87d41b5816b90f2aae62e3dc5b08d22a8746
Andrew Perotti <Perotti28@gmail.com>
2023-08-29 09:08:26 -0700

    added todays challenge which was to build a function that takes in a language and returns 'welcome' in that language or the english welcome if null or undefined however I could have created a shorter return statement that did the same process so I'll add this challenge to my anki and practice it

---
+++ welcome.js
@@ -0,0 +1,26 @@
+let languages = {
+  english: 'Welcome',
+  czech: 'Vitejte',
+  danish: 'Velkomst',
+  dutch: 'Welkom',
+  estonian: 'Tere tulemast',
+  finnish: 'Tervetuloa',
+  flemish: 'Welgekomen',
+  french: 'Bienvenue',
+  german: 'Willkommen',
+  irish: 'Failte',
+  italian: 'Benvenuto',
+  latvian: 'Gaidits',
+  lithuanian: 'Laukiamas',
+  polish: 'Witamy',
+  spanish: 'Bienvenido',
+  swedish: 'Valkommen',
+  welsh: 'Croeso'
+};
+
+function greet(language) {
+  return languages[language]||languages["english"]
+  // return languages[language] ? languages[language] : languages.english;
+}
+
+console.log(greet("italian"))
