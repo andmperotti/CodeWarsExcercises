2f09a69dcbc42285770158d36ad150b8b2c97605
Andrew Perotti <Perotti28@gmail.com>
2023-09-18 08:28:07 -0700

    Added todays challenge which was a function that takes in a full name in a string and returns the name in the opposite order

---
+++ nameShufller.js
@@ -0,0 +1,6 @@
+////https://www.codewars.com/kata/559ac78160f0be07c200005a
+function nameShuffler(str){
+  return str.split(' ').reverse().join(' ')
+}
+
+console.log(nameShuffler('Andrew Perotti'))
