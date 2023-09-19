39c60ef8d05526b9308e58555efdac18429b3c5d
Andrew Perotti <Perotti28@gmail.com>
2023-09-19 09:04:31 -0700

    Added todays challenge which was to build a function that takes in an integer value and returns the ASCII value that integer represents

---
+++ getASCIIvalue.js
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/55ad04714f0b468e8200001c
+// function getChar(c){
+//   return c.charCodeAt()
+// }
+
+// console.log(getChar('0'))
+
+function getChar(c){
+  return String.fromCharCode(c)
+}
+
+console.log(getChar('100'))
