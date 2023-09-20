52c71b691291f31af03c44ab9bcff105d691ce3c
Andrew Perotti <Perotti28@gmail.com>
2023-09-20 08:18:21 -0700

    Added todays challenge which was to build a xor function, there however is a more simplistic way to write this function and I've gone ahead and added this question to my Anki deck to further remember the syntax

---
+++ xorLogicalOperator.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/56fa3c5ce4d45d2a52001b3c
+function xor(a, b) {
+  if(a===true && b===false){
+  	return true
+  }else if(b===true && a===false){
+  	return true
+  }else{
+  	return false
+  }
+}
