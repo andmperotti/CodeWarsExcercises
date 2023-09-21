1d81abe3769456d14670bb9807f2e51f1eac3f47
Andrew Perotti <Perotti28@gmail.com>
2023-09-21 08:44:37 -0700

    Added todays challenge which would tell a user if a number argument is plural or not

---
+++ plural.js
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/52ceafd1f235ce81aa00073a
+function plural(n) {
+  return n==1 ? false : true;
+}
+
+//best practice:
+/*
+function plural(n) {
+  return n !== 1;
+}
+*/
