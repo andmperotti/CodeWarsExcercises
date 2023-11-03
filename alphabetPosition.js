62e29697ceb74cc4bce03a7fc0ba8da12d893774
Andrew Perotti <Perotti28@gmail.com>
2023-11-03 08:19:15 -0700

    Added todays challenge which was to build a function that takes in a letter and returns its position in hte alphabet

---
+++ alphabetPosition.js
@@ -0,0 +1,6 @@
+//https://www.codewars.com/kata/5808e2006b65bff35500008f
+function position(letter){
+	return `Position of alphabet: ${"abcdefghijklmnopqrstuvwxyz".indexOf(letter)+1}`
+}
+
+console.log(position('z'))
