60e4ec83b32effddfdb6723c331fb59b448c9384
Andrew Perotti <Perotti28@gmail.com>
2023-12-07 08:05:45 -0800

    Added changes to yesterdays first code challenge, I didn't look at the code I used yesterday. ?

--- smallestUnusedID.js
+++ smallestUnusedID.js
@@ -1,16 +1,28 @@
-//https://www.codewars.com/kata/55eea63119278d571d00006a
+// //https://www.codewars.com/kata/55eea63119278d571d00006a
+// function nextId(ids){
+// 	ids.sort()
+// 	console.log(ids)
+// 	for(let i = 0; i<ids.length; i++){
+// 		if(ids[i]!=i){
+// 			return i
+// 		}
+// 	}
+// 	//if above loop didn't return a value, then take the last and add 1 to it
+// 	return ids.length
+// }
+// console.log(nextId([0,1,2,3,4,5,6,7,8,9,10]))
+
+
+// //did not complete
+
+
+//second day try
 function nextId(ids){
-	ids.sort()
-	console.log(ids)
-	for(let i = 0; i<ids.length; i++){
-		if(ids[i]!=i){
+	for(let i = 0; i<=ids.length; i++){
+		if(!ids.includes(i)){
 			return i
 		}
 	}
-	//if above loop didn't return a value, then take the last and add 1 to it
-	return ids.length
+	return ids.length+1
 }
 console.log(nextId([0,1,2,3,4,5,6,7,8,9,10]))
-
-
-//did not complete
