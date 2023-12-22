7cf3a915aedc4ee3e989da0569ab8ff3d4948749
Andrew Perotti <Perotti28@gmail.com>
2023-12-22 08:43:40 -0800

    Worked on a previous challenge today, I created a function that worked but codewars kept telling me that a property was undefined even though in my editor I copied all of their testing code. I took the best practice answer and put it in my Anki deck because it was shorter than my answer although it's pretty much the same result so I'm really unsure why it didn't accept my solution.

--- duckDuckGoose.js
+++ duckDuckGoose.js
@@ -2,17 +2,22 @@
 
 function duckDuckGoose(players, goose) {
 	if(goose>players.length){
-		return (players[(goose%players.length)-1].name)
+		return players[(goose %(players.length))-1].name
 	}else{
-		return (players[goose-1].name)
+		return players[goose-1].name
 	}
-
 }
-let a = {name: 'a'}
-let b = {name: 'b'}
-let c = {name: 'c'}
-let d = {name: 'd'}
-console.log(duckDuckGoose([a, b, c, d], 4))
+
+class Player {
+  constructor(name) {
+  	this.name = name;
+  }
+}
+
+let ex_names = ["a", "b", "c", "d", "c", "e", "f", "g", "h", "z"];
+let players = ex_names.map((n) => new Player(n));
+
+console.log(duckDuckGoose(players, 18))
 
 //I apparently was wrong although this worked for me....
 //There was a much simpler way to write this which means I'll add it to my anki
