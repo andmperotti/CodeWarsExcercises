c73cc761149f08e03a94b1e3b10bd2028e5da971
Andrew Perotti <Perotti28@gmail.com>
2023-12-27 08:20:53 -0800

    Added work on a code challenge from the past, I understand what the best practice is and I was close to it, but will need to come back again later

--- multipleOfIndex.js
+++ multipleOfIndex.js
@@ -1,6 +1,7 @@
 //https://www.codewars.com/kata/5a34b80155519e1a00000009
 function multipleOfIndex(array) {
-  return array.filter((x,i) => x == 0 || x % i === 0)
+  return array.filter((ele, ind, arr)=>ele === 0 || ele%ind === 0)
 }
 
 console.log(multipleOfIndex([-49, -55, 1,2,3,4,5, 7]))
+console.log(multipleOfIndex([0, 2, 3, 6, 9], [0, 2, 6]))
