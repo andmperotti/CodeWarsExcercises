9628be3781840e716790c6bfbb4f0a57b5596b7b
Andrew Perotti <Perotti28@gmail.com>
2023-12-28 08:47:05 -0800

    Added todays challenge work which was on a previously attempted challenge, I did better but my code still didn't sufice all test cases. There was some similar codes I saw in the solutions that I will try to recall from memory the next time I attempt that will work.

--- logicalCalculator.js
+++ logicalCalculator.js
@@ -1,11 +1,51 @@
 //https://www.codewars.com/kata/57096af70dad013aa200007b
-function logicalCalc(array, op){
-  if(op = "AND"){
-  	return array.every(e=>e===true)
-  }else if(op==="OR"){
-  	return array.every((ele, ind, arr)=>ele===true || arr[ind+1]===true)
-  }else if(op==="XOR"){
-  	return arr.every((ele, ind,arr)=>(ele===true && arr[ind+1]===false)||(ele===false && arr[ind+1]===true))
+// function logicalCalc(array, op){
+//   if(op==="AND"){
+//   	return !array.includes(false)
+//   }else if(op==="OR"){
+//   	let a = array[0]
+//   	for(let i = 1; i<array.length; i++){
+//   		a = a || array[i]
+//   	}
+//   	return a
+//   }else if(op==="XOR"){
+//   	let a = array[0]
+//   	for(let i = 1; i<array.length; i++){
+//   		if(a===array[i]){
+//   			a=false
+//   		}else{
+//   			a===true
+//   		}
+//   	}
+//   	return a
+//   }
+// }
+// console.log(logicalCalc([true, true, false], "XOR"))
+
+//my attempt above did not account for passing in one element arrays with an operator
+
+//this is a similar approach by someone else that I'll try to recall at a later date:
+
+function logicalCalc(array, op)
+{ 
+  var result = array[0];
+  for(var i = 1; i < array.length; i++)
+  {
+    if(op == "AND")
+    {
+      result = result && array[i];
+    }
+    if(op == "OR")
+    {
+      result = result || array[i];
+    }
+    if(op == "XOR")
+    {
+      result = result != array[i];
+    }
   }
+  return result;
 }
-console.log(logicalCalc([true, true, true, false], "OR"))
+
+console.log(logicalCalc([true], "AND"))
+console.log(true && undefined)
