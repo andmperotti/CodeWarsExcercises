5960ad02c1c9256d2575489bc49e3c33aed648e1
Andrew Perotti <Perotti28@gmail.com>
2024-07-18 09:37:10 -0700

    Added todays challenge, which was to freeze an object.

---
+++ mrFreeze.js
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/514a3996d22ce03198000003
+
+//Argument is an object named 'MrFreeze'
+//End result will be that we freeze the object so no changes can be made to it
+
+
+Object.freeze(MrFreeze)
