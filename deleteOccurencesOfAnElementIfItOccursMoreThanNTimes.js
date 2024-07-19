b9553af6852001cb784cde7d0296f76bca3ab0be
Andrew Perotti <Perotti28@gmail.com>
2024-07-19 11:17:22 -0700

    Added todays challenge, which was a secodn attempt at the delete occurences of an element if it occurs more than n times challenge.

--- deleteOccurencesOfAnElementIfItOccursMoreThanNTimes.js
+++ deleteOccurencesOfAnElementIfItOccursMoreThanNTimes.js
@@ -5,18 +5,18 @@
 //example: ([1,1,1,2,2,3,3,3],2)=>[1,1,2,2,3,3]
 
 
-function deleteNth(arr,n){
-	//iterate over the array and build an object that holds each specific value as a property and increment its value each time an element with that value is iterated over
-	let counts = {}
-	arr.forEach((e,i,arr)=>{
-		if(!counts[e]){counts[e]=0}
-		counts[e]++
-	})
-	//return an array using the properties as the value and the value of those properties as the number of instances of that value
-	return Array.from({length: Object.keys(counts).length}, e=>counts[e])
+// function deleteNth(arr,n){
+// 	//iterate over the array and build an object that holds each specific value as a property and increment its value each time an element with that value is iterated over
+// 	let counts = {}
+// 	arr.forEach((e,i,arr)=>{
+// 		if(!counts[e]){counts[e]=0}
+// 		counts[e]++
+// 	})
+// 	//return an array using the properties as the value and the value of those properties as the number of instances of that value
+// 	return Array.from({length: Object.keys(counts).length}, e=>counts[e])
 
-}
-console.log(deleteNth([1,1,1,2,2,3,3,3], 2))
+// }
+// console.log(deleteNth([1,1,1,2,2,3,3,3], 2))
 
 //we could use the filter method and run it as many times as there are differnt values however that would take a lot of resources depending on how many different locations there are in the array.
 
@@ -44,3 +44,30 @@ console.log(deleteNth([1,1,1,2,2,3,3,3], 2))
 //Ok I understand now, there are 2 things happening inside the filter methods callback function. First it's adding/incrementing properties in the object. Secondly its returning the iteration elements value to an array that will be the final output of the entire function, but it's limiting those elements depending on how many have already been added by using the object property values.
 
 //I'll try this challenge again later this week, but am glad I came across it and dissected it here.
+
+
+
+
+
+
+
+
+
+
+//7/19 attempt, have not looked above this line today:
+
+
+
+function deleteNth(arr,n){
+	let occurences = {}
+	return arr.filter(e=>{
+		occurences[e] = (occurences[e]||0)+1
+		return occurences[e]<=n
+	})
+}
+console.log(deleteNth([1,1,1,2,2,3,3,3], 2))
+
+//build an object which will hold each unique value as a property and their values will be the occurence count of each value in the array
+//retrun the output of using map method which: we'll use to iterate over the argument array of values, add or increment the value as a property to the object 'occurences', and add the value into the array if at that time the amount of iterator value occurences is less than the limiting argument(n)
+
+//had a hiccup by using dot notation when i should have been using bracket notation, using dot notation you'd literally be setting properties of 'e', by using bracket notation we evaluate e for the value and that gets used as the property in occurences which results in integers as properties.
