03fa10684341905dc16a467fda0a4fe2485f313a
Andrew Perotti <Perotti28@gmail.com>
2024-08-02 08:18:16 -0700

    Added some retrys on past challenges.

--- abbreviateAtwoWordName.js
+++ abbreviateAtwoWordName.js
@@ -6,3 +6,39 @@ function abbrevName(name){
   return `${first}.${second}`
 }
 console.log(abbrevName('andrew perotti'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//8/2 reattempt, did not look at anything above this line:
+
+//argument is a string consisting of a first and last name space separated, their case could be upper or lower
+//output needs to be the first letter of each name capitalized and these separated by a period
+//example: (Sam Harris)=> 'S.H"'
+
+function abbrevName(name){
+  return name.split(' ').map(e=>e.slice(0,1).toUpperCase()).join('.')
+}
+//we could split the string into an array
+//slice each element in the array to just consist of their first letter, and uppercase that letter
+//join the 2 elements together using join('.') which inserts a period between the two
+//return that string
--- beginnerClock.js
+++ beginnerClock.js
@@ -7,3 +7,46 @@ function past(h, m, s){
   return secondsPast*1000
 }
 console.log(past(0, 1, 1))
+
+
+
+
+
+
+
+
+
+
+
+
+//retry on 8/2, have not looked at above code
+
+//arguments are 3 integers, seemingly whole, representing hours, minutes, and seconds since midnight
+//ouput will be the combined number of milliseconds since midnight by converting the arguments to milliseconds and summing them 
+//example: past(0, 1, 1)=> 61000
+
+
+
+function past(h, m, s){
+  m+= h*60
+  s+= m*60
+  return s*1000
+}
+
+//return result of adding together the:  h argument * the amount of milliseconds in an hour, m argument * the amount of milliseconds in a minute, s argument * the amount of milliseconds in a second
+
+//google gave an exponent when I asked for milliseconds in an hour, so we'll go another way
+
+//we'll convert h argument to minutes and add it to m, and then convert m to seconds and add it to s, and then return s * milliseconds in a second (1000)
+
+
+
+
+//this worked, but a more declarative syntax would have been:
+// function past(h, m, s){
+//   return ((h*3600)+(m*60)+s)*1000;
+// }
+//I'll add this to my Anki.
+//here it returns the result of calculating the seconds for the amount of hours (h) + the amount of seconds for the amount of minutes (m) + (s) seconds TIMES milliseconds in each second.
+
+
